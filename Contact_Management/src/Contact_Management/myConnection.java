5ad2f9dbfe78d35bec04d577e0995c19c32f1ca7
lam <2251120222@ut.edu.vn>
2024-11-24 07:58:36 +0700

    update

--- Contact_Management/src/Contact_Management/myConnection.java
+++ Contact_Management/src/Contact_Management/myConnection.java
@@ -17,7 +17,7 @@ public class myConnection {
         Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost/javacontactsapp", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javacontactsapp", "root", "");
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
